34f2479183bea371bfe033e53637f3d2932e1924
simoneroncallo <simoneroncallo1996@gmail.com>
2026-06-23 08:47:44 +0200

    Update dotfiles

---
+++ dotfiles/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+# Hide welcome message
+set -g fish_greeting ""
+
+# User defined aliases
+alias ll="eza -laH --inode --icons"
+alias pen="gnome-text-editor"
